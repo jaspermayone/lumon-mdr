f6191c47510f1086fb6c008174a3efa7ff6619bb
Jasper Mayone <me@jaspermayone.com>
2025-04-28 23:33:47 -0400

    Initial Commit

---
+++ mdr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mdr
+//
+//  Created by Jasper Mayone on 4/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mdr/mdrApp.swift
@@ -0,0 +1,17 @@
+//
+//  mdrApp.swift
+//  mdr
+//
+//  Created by Jasper Mayone on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct mdrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
